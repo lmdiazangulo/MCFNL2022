ee9c94c6abcab6fe87a929cb13545037b0677f42
Luis Manuel Diaz Angulo <LM@DiazAngulo.com>
2022-03-16 14:32:08 +0100

    FFT exercise

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import numpy.fft as npf
 
+tIni = 0
+tEnd = 500e-6
+N = int(1e5 + 1)
 
-t = np.linspace(0, 10)
+s0 = 1e-6
+t0 = 10*s0
 
-plt.plot(t, np.sin(t))
+t = np.linspace(tIni, tEnd, N)
+g = np.exp(- np.power(t - t0, 2)/(2 * s0**2))
+
+fq = npf.fftshift(npf.fftfreq(N, t[1]-t[0]))
+
+plt.plot(fq, npf.fftshift(np.abs(npf.fft(g))))
+plt.xlim(-100e3, 100e3)
 
 plt.show()
+
+print("END")
