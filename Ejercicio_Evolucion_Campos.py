4613b2d59ec0c683d8ba75a6b1f86b5bc6e83694
Luis Manuel Diaz Angulo <LM@DiazAngulo.com>
2022-03-29 14:00:22 +0200

    Works

--- Ejercicio_Evolucion_Campos.py
+++ Ejercicio_Evolucion_Campos.py
@@ -17,7 +17,7 @@ N = 101
 mean = (xMax + xMin)/2#media de la gaussiana
 spread = ((xMax - xMin)/10)
 
-tEnd = 2.5
+tEnd = 10.0
 
 
 
@@ -29,15 +29,15 @@ grid = np.linspace(xMin,xMax,N)#grid primario
 dualGrid = (grid[:-1]+grid[1:])/2#grid dual
 dx = grid[1]-grid[0]#cualquiera dos puntos consecutivos serviría
 dt = 0.8*dx/speedOfLight
-dimt = round(tEnd/dt)#numero de filas (tiempos)
+dimt = round(tEnd/dt)+2#numero de filas (tiempos)
 
 Eold = np.exp( -np.power(grid - mean,2)/2.0/np.sqrt((spread)))#condición inicial
 Hold = dualGrid*0.0#condición lineal para H
 
-Enew = np.zeros([dimt,101])
-Hnew = np.zeros([dimt,100])
+E = np.zeros([dimt,101])
+H = np.zeros([dimt,100])
 
-Enew[0][:] = Eold
+E[0][:] = Eold
 
 
 
@@ -45,13 +45,12 @@ t = 0.0
 i = 1
 while t<tEnd:
 
- Enew[i][1:-1] = -(dt/dx)*(Hnew[i-1][1:] - Hnew[i-1][:-1]) + Enew[i-1][1:-1]
- Hnew[i][:] = - (dt/dx)*(Enew[i-1][1:] - Enew[i-1][:-1]) + Hnew[i-1][:]
+    E[i][1:-1] = -(dt/dx)*(H[i-1][1:] - H[i-1][:-1]) + E[i-1][1:-1]
+    E[i][-1] = -(dt/dx)*(- 2.0 * H[i-1][-1])+  E[i-1][-1]
+    H[i][:] = - (dt/dx)*(E[i][1:] - E[i][:-1]) + H[i-1][:]
 
- t += dt
- i += 1
- if i == 31:
-     break
+    t += dt
+    i += 1
 
 
 #Create figure
@@ -62,8 +61,8 @@ ax = fig.add_subplot(111)
 #funcion animacion
 def actualizar(i):
     ax.clear()
-    ax.plot(grid,Enew[i,:],color = "darkblue")
-    ax.plot(dualGrid,Hnew[i,:], color = "red")
+    ax.plot(grid,E[i,:],color = "darkblue")
+    ax.plot(dualGrid,H[i,:], color = "red")
     ax.set_ylim(-1,1)#esto hace que el eje y no se mueva en la animación
 
 
@@ -73,7 +72,8 @@ ani = animation.FuncAnimation(fig,actualizar,range(dimt))
 
 
 
-
+plt.show()
+print("END")
 
 
 
